f4f01c0f03b1fa6da511ed1d18733ae959265d11
anchalexcellence <anchal@excellencetechnologies.in>
2017-04-19 15:11:26 +0530

    table directive

--- app/app.js
+++ app/app.js
@@ -3,21 +3,11 @@ app.config(function ($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/outletViewTable')
 
   $stateProvider
-        .state('register', {
-          url: '/register',
-          templateUrl: 'components/registration/registrationForm.html',
-          controller: 'registrationController'
-        })
-.state('outletViewTable', {
-  url: '/outletViewTable',
-  templateUrl: 'components/outlets/outletView.html',
-  controller: 'viewOutletController',
-  controllerAs:'OutletCtrl'
+  .state('outletViewTable', {
+    url: '/outletViewTable',
+    templateUrl: 'components/outlets/outletView.html',
+    controller: 'viewOutletController',
+    controllerAs:'OutletCtrl'
 
-})
-        .state('login', {
-          url: '/login',
-          templateUrl: 'components/login/login.html'
-
-        })
+  })
 })
