17d9aea107f77464a2db4ac4a13a1e8d8f7e64b3
ashish <ashish_singh@excellencetechnologies.in>
2017-04-20 18:37:07 +0530

    commit

---
+++ app/common/ajaxRequest.factory.js
@@ -0,0 +1,8 @@
+var app = angular.module("batApp");
+app.factory("getDataFactory", function(configuration, $resource) {
+    return {
+        sendData: function(url) {
+            return $resource(configuration.apihost + url)
+        }
+    }
+});
---
+++ app/common/interceptor.js
@@ -0,0 +1,10 @@
+var app = angular.module("batApp");
+app.factory('Interceptor', function() {
+    return {
+        request: function(config) {
+            return config;
+			console.log("cjnsjkncd")
+
+        }
+    }
+});
