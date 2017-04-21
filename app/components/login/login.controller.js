1d58982184b6f35cb36d3f40c450023776ac14ab
ashish <ashish_singh@excellencetechnologies.in>
2017-04-21 15:20:19 +0530

    loginForm

---
+++ app/components/login/login.controller.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var app = angular.module('batApp')
+app.controller('loginController', function ($localStorage, getDataFactory, $state) {
+  this.submit = function (data) {
+    var url = '/admin/login'
+    getDataFactory.sendData(url).save(data).$promise
+.then((response) => {
+  if (!response.error) {
+    $localStorage.token = response.token
+    $state.go('template')
+  }
+})
+  }
+})
