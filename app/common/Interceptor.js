c927aed6eaaadd90ad5a4035686e4cd4a723b728
anchalexcellence <anchal@excellencetechnologies.in>
2017-04-25 17:20:52 +0530

    resolve comment

--- app/common/Interceptor.js
+++ app/common/Interceptor.js
@@ -4,8 +4,6 @@ angular.module('batApp')
      request: function (config) {
        if ($localStorage.token) {
          config.url = config.url + '?accessToken=' + $localStorage.token
-       } else {
-         config.url = config.url
        }
        return config
      }
