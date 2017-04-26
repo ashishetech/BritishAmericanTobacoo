cf18006e6138f520066c9fa628e1f3b5eff6de76
ashish <ashish_singh@excellencetechnologies.in>
2017-04-26 11:19:31 +0530

    implemented side menu in batApp

--- app/components/login/LoginController.js
+++ app/components/login/LoginController.js
@@ -1,22 +1,21 @@
 'use strict'
 angular.module('batApp')
-    .controller('LoginController', function ($localStorage, getDataFactory, $state) {
-      var vm = this
-	  vm.isLoading = false;
-	  vm.buttonLogin=true;
-      vm.submit = function (data) {
-		vm.isLoading = true;
-		vm.buttonLogin=false;
-        getDataFactory.login().save(data).$promise
-            .then((response) => {
-              if (!response.error) {
-                $localStorage.token = response.token
-                $state.go('menuTemplate')
-              }
-			  else {
-				  vm.isLoading = false;
-				  vm.buttonLogin=true;
-			  }
-            })
-      }
+    .controller('LoginController', function($localStorage, getDataFactory, $state) {
+        var vm = this
+        vm.isLoading = false;
+        vm.buttonLogin = true;
+        vm.submit = function(data) {
+            vm.isLoading = true;
+            vm.buttonLogin = false;
+            getDataFactory.login().save(data).$promise
+                .then((response) => {
+                    if (!response.error) {
+                        $localStorage.token = response.token
+                        $state.go('menuTemplate')
+                    } else {
+                        vm.isLoading = false;
+                        vm.buttonLogin = true;
+                    }
+                })
+        }
     })
