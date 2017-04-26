28f9924ef9a490fc5010621f0d403fd6dbce3bdf
ashish <ashish_singh@excellencetechnologies.in>
2017-04-26 12:14:43 +0530

    conflict resolved and code formatted

--- app/batApp.js
+++ app/batApp.js
@@ -1,22 +1,22 @@
 'use strict'
 
 angular.module('batApp', ['ui.router', 'ngResource', 'ngStorage', 'ngMaterial'])
-    .config(function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/login')
-        $stateProvider
+    .config(function ($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise('/login')
+      $stateProvider
             .state('menuTemplate', {
-                url: '/menuTemplate',
-                templateUrl: 'components/template/menuTemplate.html',
-                controller: 'TemplateController as template'
+              url: '/menuTemplate',
+              templateUrl: 'components/template/menuTemplate.html',
+              controller: 'TemplateController as template'
             })
             .state('login', {
-                url: '/login',
-                templateUrl: 'components/login/login.html',
-                controller: 'LoginController as login'
+              url: '/login',
+              templateUrl: 'components/login/login.html',
+              controller: 'LoginController as login'
             })
             .state('outletViewTable', {
-                url: '/outletViewTable',
-                templateUrl: 'components/outlets/outletView.html',
-                controller: 'viewOutletController as batCtrl'
+              url: '/outletViewTable',
+              templateUrl: 'components/outlets/outletView.html',
+              controller: 'viewOutletController as batCtrl'
             })
     })
--- app/components/template/TemplateController.js
+++ app/components/template/TemplateController.js
@@ -1,45 +1,45 @@
-angular.module('batApp').controller('TemplateController', ['$location', function($location) {
-    var vm = this
-    vm.OpenSetting = false;
-    vm.OpenOutlet = false;
-    vm.OpenBrand = false;
-    vm.arrowOutlet = true;
-    vm.arrowSet = true;
-    vm.arrowBrand = true;
+angular.module('batApp').controller('TemplateController', ['$location', function ($location) {
+  var vm = this
+  vm.OpenSetting = false
+  vm.OpenOutlet = false
+  vm.OpenBrand = false
+  vm.arrowOutlet = true
+  vm.arrowSet = true
+  vm.arrowBrand = true
 
-    vm.Open = function(count) {
-        switch (count) {
-            case '1':
-                if (vm.OpenOutlet == false) {
-                    vm.OpenOutlet = true;
-                    vm.arrowOutlet = false;
-                } else {
-                    vm.OpenOutlet = false;
-                    vm.arrowOutlet = true;
-                }
-                break;
-            case '2':
-                if (vm.OpenBrand == false) {
-                    vm.OpenBrand = true;
-                    vm.arrowBrand = false;
-                } else {
-                    vm.OpenBrand = false;
-                    vm.arrowBrand = true;
-                }
-                break;
-            case '3':
-                if (vm.OpenSetting == false) {
-                    vm.OpenSetting = true;
-                    vm.arrowSet = false;
-                } else {
-                    vm.OpenSetting = false;
-                    vm.arrowSet = true;
-                }
+  vm.Open = function (count) {
+    switch (count) {
+      case '1':
+        if (vm.OpenOutlet === false) {
+          vm.OpenOutlet = true
+          vm.arrowOutlet = false
+        } else {
+          vm.OpenOutlet = false
+          vm.arrowOutlet = true
+        }
+        break
+      case '2':
+        if (vm.OpenBrand === false) {
+          vm.OpenBrand = true
+          vm.arrowBrand = false
+        } else {
+          vm.OpenBrand = false
+          vm.arrowBrand = true
+        }
+        break
+      case '3':
+        if (vm.OpenSetting === false) {
+          vm.OpenSetting = true
+          vm.arrowSet = false
+        } else {
+          vm.OpenSetting = false
+          vm.arrowSet = true
         }
     }
+  }
 
-    vm.isActive = function(viewLocation) {
-        var Active = (viewLocation === $location.path());
-        return Active;
-    };
+  vm.isActive = function (viewLocation) {
+    var Active = (viewLocation === $location.path())
+    return Active
+  }
 }])
