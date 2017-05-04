759aa09f2f4eee130fa29d3c42b9b127298508ac
ashish <ashish_singh@excellencetechnologies.in>
2017-05-04 16:47:14 +0530

    format codes

--- app/components/membership/editMembership/EditMembershipController.js
+++ app/components/membership/editMembership/EditMembershipController.js
@@ -1,29 +1,23 @@
-angular.module("batApp")
-.controller("EditMembershipController", function($localStorage, $state, getDataFactory,$timeout){
-	var vm=this;
-	vm.id=101;
-	vm.editMembership=function () {
-		getDataFactory.editMembership(vm.id).get().$promise
-		.then((response)=>{
-				console.log(response)
-				vm.data=response;
-		})
-	}
+angular.module('batApp')
+.controller('EditMembershipController', function ($localStorage, $state, getDataFactory, $timeout) {
+  var vm = this
+  // vm.id = 101
+  vm.editMembership = function () {
+    getDataFactory.editMembership(vm.id).get().$promise
+.then((response) => {
+  vm.data = response
+})
+  }
 
-    vm.submit = function(data) {
-		console.log(data);
-
-		getDataFactory.updateMembership(vm.id).update(data).$promise
-		.then((response)=>{
-				console.log(response)
-				if (!response.error) {
-   					angular.element('#myModal').modal('hide')
-				}
-				else {
-					vm.data.error=response.error.message
-				}
-		})
-
-    };
-	vm.editMembership();
-});
+  vm.submit = function (data) {
+    getDataFactory.updateMembership(vm.id).update(data).$promise
+.then((response) => {
+  if (!response.error) {
+    angular.element('#myModal').modal('hide')
+  } else {
+    vm.data.error = response.error.message
+  }
+})
+  }
+  vm.editMembership()
+})
