7cb097444d6bf38ea64c1f65fad134cd152286d1
anchalexcellence <anchal@excellencetechnologies.in>
2017-05-10 14:29:10 +0530

    brandView updated

--- app/components/addTme/addTmeController.js
+++ app/components/addTme/addTmeController.js
@@ -1,23 +1,18 @@
 'use strict'
 angular.module('batApp')
-    .controller('addTmeController', function (getDataFactory, $state) {
-      var vm = this
+.controller('addTmeController', function (getDataFactory, $state) {
+  var vm = this
+  vm.err = false
 
-
-      vm.save = function (data) {
-        console.log(data)
-        getDataFactory.addTme().save(data).$promise
-                .then((response) => {
-                if (!response.error) {
-                  $state.go('menuTemplate.tmeViewTable')
-                  }
-                  else{
-                      vm.errmsg=response.error.message
-                    console.log(response.error)
-                  }
-                })
-      }
-         vm.change = function () {
-                    vm.errmsg=''
-      }
-       })
+  vm.save = function (data) {
+    getDataFactory.addTme().save(data).$promise
+.then((response) => {
+  if (!response.error) {
+    $state.go('menuTemplate.tmeViewTable')
+  }
+})
+  }
+  vm.change = function () {
+    vm.err = false
+  }
+})
